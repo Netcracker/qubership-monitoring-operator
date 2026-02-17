d50203c018d80a3934e1d149a582c0e40cbd600a
NikolaiKuziaevQubership <nikolai.kuziaev.qubership@gmail.com>
2026-02-17 18:48:48 +0700

    feat: minor fixes to issues and behavior in Grafana. Go panic have been fixed. Datasource separation has been added for Grafana. Rollback for some parts

--- controllers/grafana-operator/manifest.go
+++ controllers/grafana-operator/manifest.go
@@ -12,7 +12,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
