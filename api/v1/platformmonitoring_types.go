f47356877285a524b57763108b7074d3f2a5b438
Alexey Karasev <oneasat@gmail.com>
2026-08-26 16:43:02 +0500

    chore: replace deprecated schema.Builder (#380)
    * chore: replace deprecated schema.Builder
    
    * fix: update api to use new builder
    
    * chore: fix issue found by editorconfig linter

--- api/v1/platformmonitoring_types.go
+++ api/v1/platformmonitoring_types.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -1798,6 +1798,10 @@ type PrometheusRules struct {
 type Promxy struct {
 	Install *bool  `json:"install,omitempty"`
 	Port    *int32 `json:"port,omitempty"`
+}
+
+func init() {
+	objectTypes = append(objectTypes, &PlatformMonitoring{}, &PlatformMonitoringList{})
 }
 
 // IsInstall check if AlertManager should be installed
